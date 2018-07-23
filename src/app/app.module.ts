b0090473c482933f3ac3fbe017c1905ac5a11810
Jorge Francisco <franciscomigueljorge@gmail.com>
2018-07-23 12:35:30 -0500

    rutas agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {app_routing} from "./app.routes";
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -10,15 +13,11 @@ import { ProductoComponent } from './components/producto/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    PortafolioComponent,
-    AboutComponent,
-    ProductoComponent
+    AppComponent, HeaderComponent, FooterComponent, PortafolioComponent, AboutComponent, ProductoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
