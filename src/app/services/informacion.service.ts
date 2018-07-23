9b7ceae20cf7be683d6abbc13a22f16fe6e31404
Jorge Francisco <franciscomigueljorge@gmail.com>
2018-07-23 15:01:58 -0500

    serivicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,14 +7,32 @@ import { HttpClient } from "@angular/common/http";
 
 export class InformacionService {
   info:any = {};
+  equipo:any = [];
+
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
 
   constructor(private http: HttpClient) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+  public carga_info(){
+
     this.http.get("assets/data/info.pagina.json")
       .subscribe(data => {
         this.cargada = true;
         this.info = data;
+      });
+
+  }
+  carga_sobre_nosotros(){
+
+    this.http.get("https://paginaweb-a3abc.firebaseio.com/equipo.json")
+      .subscribe(data => {
+        this.cargada = true;
+        this.info = data;
         console.log(data);
+        this.cargada_sobre_nosotros = true;
       });
   }
 
